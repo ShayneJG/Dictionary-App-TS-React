3544181f1a4ed67306894a7a911fe550017957fd
Shayne Geilman <shaynegeilman@gmail.com>
2023-03-19 16:08:13 +0800

    search component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Settings from "./components/settings";
+import Search from "./components/search";
 
 const App = () => {
   //interfaces
 
   //states
-
+  const [search, setSearch] = useState<string>("");
   const [font, setFont] = useState<string>("sans serif");
   const [night, setNight] = useState<boolean>(false);
 
   return (
     <div id="app-container" className="w-1/2 m-auto">
       <Settings setFont={setFont} setNight={setNight} night={night} />
+      <Search search={search} setSearch={setSearch} />
     </div>
   );
 };
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,3 +1,29 @@
-const Search = () => {
-  return <div></div>;
+//interfaces
+
+import { ReactElement } from "react";
+
+interface SearchProps {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Search: React.FunctionComponent<SearchProps> = ({
+  search,
+  setSearch,
+}) => {
+  function changeHandle(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(e.currentTarget.value);
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        className="border border-solid"
+        value={search}
+        onChange={changeHandle}
+      />
+    </div>
+  );
 };
+
+export default Search;
