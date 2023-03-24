a8e58bcf0fe54c3b570bbb7db3087f6566b4445e
Shayne Geilman <shaynegeilman@gmail.com>
2023-03-24 23:21:59 +0800

    word styled

--- src/components/definition.tsx
+++ src/components/definition.tsx
@@ -21,12 +21,14 @@ const Definition: React.FC<DefinitionProps> = ({ word, fetchWord }) => {
 
   return (
     <div>
-      <div id="header">
-        <div id="word">
-          <h1>{word.word}</h1>
-          <h2>{word.phonetic}</h2>
+      <div className="flex justify-between mt-5 mb-5" id="header">
+        <div>
+          <h1 className="font-bold text-[32px] text-[#2d2d2d] mb-2">
+            {word.word}
+          </h1>
+          <h2 className=" text-[#A445ed]">{word.phonetic}</h2>
         </div>
-        <img src={playIcon}></img>
+        <img className="h-12 mt-auto mb-auto" src={playIcon}></img>
       </div>
       <div id="parts-of-speech">
         {word.meanings.map((meaning) => (
